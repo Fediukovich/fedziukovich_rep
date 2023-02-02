486e05a4d039be12477b537c2cfadc7d8034d536
Fediukovich <artem.fediukovich@gmail.com>
2023-02-02 14:15:41 +0300

    les1

---
+++ les1.py
@@ -0,0 +1,83 @@
+#print('hello world')
+#print("hello world")
+#print('''hello world''')
+
+#first_name = "Alex"
+#print('hello', first_name, sep='---',end='')
+#print('hello', first_name)
+
+#first_name = input('enter your first name:')
+#print(first_name)
+
+#a = 4
+#b = 2
+#c = a//b
+#print (c)
+
+#a = 4
+#b = 2
+#c = a**b
+#print (c)
+
+#a = 5
+#b = 2
+#c = a % b
+#print (c)
+
+# a = 5
+# b = 2
+# a += b
+# print (a)
+
+# a = 23.5
+# b = 24.5
+# print(round(a))
+# print (round(b))
+
+# f_name = 'Artsiom'
+# l_name = ' Fedziukovich'
+# full_n = f_name + l_name
+# print(full_n)
+# print(f_name)
+
+# age = input('enter age: ')
+# age = float(age)
+# print(age)
+# print(type(age))
+
+# a = 100_000_000
+# b = str(a)
+# print(b)
+# print(type(b))
+#
+# text = 'hello world'
+# print(len(text))
+# print(text[6])
+# print(text[-5])
+
+#print('hello \n world')
+#print('hello \n \t world')
+#print(r'hello \n \t world')
+
+# name = 'Alex'
+# age = 35
+# text = 'hello {} age {}'.format(name, age)
+# text = f'hello {name} age {age}'
+# print(text)
+#
+# text = 'hello---python-world'
+# words = text.split('---', 1)
+# print(words)
+#
+# words = ['hello', 'python', 'world']
+# text = '|'.join(words)
+# print(words)
+#
+# text = 'hello python world'
+# text = text.replace('','---')
+# print(text)
+
+#text = 'hello'
+#print(text.startswith('Hel'))
+# text = 'hello'
+# print(text.eith('Hel'))
