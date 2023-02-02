b225deba800bcc91b50379b77345a4de9a33de51
Fediukovich <artem.fediukovich@gmail.com>
2023-02-02 14:02:02 +0300

    les2

---
+++ les2.py
@@ -0,0 +1,34 @@
+
+# text = 'Hello world python world hello'
+# print(text.find('world', 8, 15))
+# print(text.rfind('world'))
+
+# print(text.index('World'))
+# print(text.rindex('world'))
+
+# print(text.count('world, 8, 15'))
+
+# print(text.partition('world'))
+# print(text.rpartition('world'))
+#
+# print(text.islower())
+# print(text.isupper())
+# print(text.istitle())
+
+# text2 = text.capitalize()
+# text2 = text.swapcase()
+# text2 = text.casefold
+
+# text = ' \n\thello\t\tworld \n\t'
+# print(text.lstrip())
+# print(text.rstrip())
+# print(text.strip())
+#
+# text = 'hello\tworld\tpython'
+# print(text.expandtabs(15))
+#
+# print('hello'.center(12, '-'))
+# print('hello'.zfill(7))
+#
+# print('hello'.ljust(7, '-'))
+print('hello'.rjust(7, '-'))
