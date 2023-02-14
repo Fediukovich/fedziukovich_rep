7bd2df63b0a3d5fec977b8499bc04b790b294830
Fediukovich <artem.fediukovich@gmail.com>
2023-02-14 16:01:16 +0300

    lesson5

---
+++ Lessons/les5.py
@@ -0,0 +1,16 @@
+# def is_palindrome(text):
+#     text = text.lower().strip().replace(' ', '')
+#     print(text == text[::-1])
+#
+# is_palindrome('шалаш')
+# is_palindrome('python')
+#
+# def is_palindrome(text):
+#     text = text.lower().strip().replace(' ', '')
+#     return text == text[::-1], text
+#
+# a = is_palindrome('шалаш')
+# b = is_palindrome('python')
+# print(a)
+# print(b)
+
